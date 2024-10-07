52e6b3a994cdbb4e557389c7ac35eb6060ea9969
Brandon L. Canete <brandoncanete35@gmail.com>
2024-10-07 22:16:29 +0800

    Initial Commit : App Component

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppComponent} from "./app.component";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+    {path: '', component: AppComponent},
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(routes, {enableTracing: true}),
+        FormsModule,
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    providers: [
+        provideAnimationsAsync(),
+    ],
+        
+    bootstrap: [
+        AppComponent,
+    ],
+    exports: [RouterModule],
+})
+export class AppModule{} 
